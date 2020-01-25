288935636d01a42ef2fe967ee829e924acf6ad46
Antônio Carlos <antoniocarlos.dev@gmail.com>
2020-01-25 18:30:03 +0100

    KK-2
    * setting up Firebase project + Crashlytics

--- KickerKill/Application/AppDelegate.swift
+++ KickerKill/Application/AppDelegate.swift
@@ -1,6 +1,7 @@
 // Copyright © 9/7/19 Antônio Carlos. All rights reserved.
 
 import UIKit
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -9,9 +10,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
+        FirebaseApp.configure()
+
         let initialViewController = BaseNavigationController()
         initialViewController.setRootWireframe(SoundEffectsWireFrame())
-        
+
         self.window = UIWindow(frame: UIScreen.main.bounds)
         self.window?.rootViewController = initialViewController
         self.window?.makeKeyAndVisible()
