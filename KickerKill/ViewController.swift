5accc0f9356c34ed587647d438f65d0ae6217b7a
Antônio Carlos <antoniocarlos.dev@gmail.com>
2020-07-16 18:49:28 +0200

    Initial Commit

---
+++ KickerKill/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KickerKill
+//
+//  Created by Antonio Carlos on 16.07.20.
+//  Copyright © 2020 TNTStudios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
