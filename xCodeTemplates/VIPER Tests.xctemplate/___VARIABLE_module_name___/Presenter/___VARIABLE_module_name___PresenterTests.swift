d0b558f9de9187d7f171be1060316ae60f97fb13
Antonio Carlos <antoniocarlos.dev@gmail.com>
2021-02-18 15:41:30 -0300

    Creating the Tests module builder
    - Criando templates de testes unitários para o VIPER
    	- Presenter ( adicionando restante )

--- xCodeTemplates/VIPER Tests.xctemplate/___VARIABLE_module_name___/Presenter/___VARIABLE_module_name___PresenterTests.swift
+++ xCodeTemplates/VIPER Tests.xctemplate/___VARIABLE_module_name___/Presenter/___VARIABLE_module_name___PresenterTests.swift
@@ -1,12 +1,36 @@
 //___FILEHEADER___
 
 import XCTest
-@testable import KickerKill
+@testable import ___VARIABLE_testable_target___
 
 final class ___VARIABLE_module_name___PresenterTests: XCTestCase, ___VARIABLE_module_name___ViewOutput, ___VARIABLE_module_name___InteractorOutput {
 
     var sut: ___VARIABLE_module_name___Presenter!
     var router: Mock___VARIABLE_module_name___RouterInput!
+    var interactor: Mock___VARIABLE_module_name___InteractorInput!
+    var view: Mock___VARIABLE_module_name___ViewInput!
+
+    override func setUp() {
+        super.setUp()
+
+        router = Mock___VARIABLE_module_name___RouterInput()
+        interactor = Mock___VARIABLE_module_name___InteractorInput()
+        view = Mock___VARIABLE_module_name___ViewInput()
+
+        sut = ___VARIABLE_module_name___Presenter()
+        sut.router = router
+        sut.interactor = interactor
+        sut.view = view
+    }
+
+    override func tearDown() {
+        router = nil
+        interactor = nil
+        view = nil
+        sut = nil
+
+        super.tearDown()
+    }
 
     // MARK: - ___VARIABLE_module_name___ViewOutput
 
